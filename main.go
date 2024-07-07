899e9c1458c2d95355004d1a25f3f7358f2c12df
Sean Boaden <sean.b0153@gmail.com>
2024-07-07 15:26:26 +0800

    gorillamuxadapterv2

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"example.com/fuel/routes"
 	"github.com/gorilla/mux"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 )
 
-var gorillaLambda *gorillamux.GorillaMuxAdapterV2
+var adapter *gorillamux.GorillaMuxAdapterV2
 
 func Init() {
 	r := mux.NewRouter()
 
 	// 404 handler
 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Not found: [%s]\n", r.RequestURI)
 		http.Error(w, fmt.Sprintf("Not found: [%s]\n", r.RequestURI), http.StatusNotFound)
 	})
 
@@ -33,11 +36,17 @@ func Init() {
 		w.Write([]byte("Missing ?radius={value} in URL.\n"))
 	}).Methods("GET")
 
-	// Create GorillaMuxAdapterV2 from router
-	gorillaLambda = gorillamux.NewV2(r)
+	// Create Adapter from router
+	adapter = gorillamux.NewV2(r)
+}
+
+func Handler(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	fmt.Printf("Request received through path [%s]", event.RawPath)
+	return adapter.ProxyWithContext(ctx, event)
 }
 
 func main() {
+	fmt.Printf("main()\n")
 	Init()
-	lambda.Start(gorillaLambda.ProxyWithContext)
+	lambda.Start(Handler)
 }
