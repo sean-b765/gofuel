e8531ec319a53289eeb98b13b2f8f409191df3b2
sean-b765 <sean.b0153@gmail.com>
2026-08-08 18:02:51 +0800

    hotfix: oidc role

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,9 +1,13 @@
 variable "base_path" {
   description = "API route prefix stripped by the Lambda adapter"
   type        = string
-  default     = "/perthfuel"
+  default     = "/gofuel"
 }
-
+variable "gh_repo" {
+  description = "GitHub repo, like 'org_name/repo_name'"
+  type        = string
+  default     = "sean-b765/gofuel"
+}
 variable "allowed_origin" {
   description = "Origin allowed by API Gateway CORS"
   type        = string
