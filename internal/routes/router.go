1e2417ec053a29fe78fadbdfde0f5e03fb2aac6b
sean-b765 <sean.b0153@gmail.com>
2026-08-12 12:34:04 +0800

    fix: item fields

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -11,7 +11,7 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://gofuel.seanboaden.dev", "http://localhost:3000"},
+		AllowOrigins:     []string{"https://gofuel.seanboaden.dev", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
